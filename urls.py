f7d1befb57cc0f59593adec3d0ff72035f3155a1
yourashu2005 <166690302+yourashu2005@users.noreply.github.com>
2025-07-14 19:23:03 +0530

    Code for Detecting Fake reviews

---
+++ urls.py
@@ -0,0 +1,46 @@
+"""MovieRecommenderSystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from  .import views as mainView
+from admins import views as admins
+from users import views as usr
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", mainView.index, name="index"),
+    path("index/", mainView.index, name="index"),
+    path("AdminLogin/", mainView.AdminLogin, name="AdminLogin"),
+    path("UserLogin/", mainView.UserLogin, name="UserLogin"),
+    path("UserRegister/", mainView.UserRegister, name="UserRegister"),
+
+    #adminviews
+    path("AdminLoginCheck/", admins.AdminLoginCheck, name="AdminLoginCheck"),
+    path("AdminHome/", admins.AdminHome, name="AdminHome"),
+    path('RegisterUsersView/', admins.RegisterUsersView, name='RegisterUsersView'),
+    path('ActivaUsers/', admins.ActivaUsers, name='ActivaUsers'),
+
+
+    #User Views
+    path("UserRegisterActions/", usr.UserRegisterActions, name="UserRegisterActions"),
+    path("UserLoginCheck/", usr.UserLoginCheck, name="UserLoginCheck"),
+    path("UserHome/", usr.UserHome, name="UserHome"),
+    path("DatasetView/", usr.DatasetView, name="DatasetView"),
+    path("ML_training/", usr.ML_training, name="ML_training"),
+    path("prediction/", usr.prediction, name="prediction"),
+
+]
